2a36a3e9eede687c1fc48056545cd233ff42f671
mehdi <powergame_coder2@yahoo.com>
2020-11-05 01:41:00 -0500

    Customized theme font and added top app bar

--- app/src/main/java/me/mehdi/mymoney/MainActivity.kt
+++ app/src/main/java/me/mehdi/mymoney/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -14,10 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawShadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageAsset
 import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import me.mehdi.mymoney.ui.MyMoneyTheme
@@ -33,23 +39,25 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+
+
 @Preview
 @Composable
 fun HomeScreen(){
-    MyMoneyTheme(){
-//        Column(modifier = Modifier.fillMaxSize().padding(16.dp).drawShadow(8.dp).clip(
-//            RoundedCornerShape(4.dp))){
-//            Text(modifier = Modifier.align(Alignment.CenterHorizontally) , text = "Title")
-//            Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}){
-//                Text("Let's Go")
-//            }
-//        }
-
-        Scaffold(floatingActionButton = {  }){
-        Column(modifier = Modifier.fillMaxSize().drawShadow(8.dp).clip(
-            RoundedCornerShape(4.dp))){
-            Text(modifier = Modifier.align(Alignment.CenterHorizontally).clip(RoundedCornerShape(16.dp)) , text = "My Money")
-            Text("Here Are Your Costs")
+    val scaffoldState = rememberScaffoldState()
+    MyMoneyTheme{
+        Scaffold(floatingActionButton = { FAB() },
+                topBar = { topBar() },
+                scaffoldState = scaffoldState,
+                drawerElevation = 8.dp,
+                drawerBackgroundColor = Color.Red,
+                drawerGesturesEnabled = true,
+                drawerScrimColor = Color.Green,
+                drawerContent = { HomeDrawer() }
+        ){
+        Column(modifier = Modifier.fillMaxSize().drawShadow(8.dp)){
+            Text(modifier = Modifier.align(Alignment.CenterHorizontally).clip(RoundedCornerShape(16.dp)), text = stringResource(id = R.string.app_name))
+            Text(stringResource(id = R.string.here_is_your_costs))
         }
         }
     }
@@ -58,6 +66,41 @@ fun HomeScreen(){
 @Composable
 fun FAB(){
     FloatingActionButton(onClick = {}) {
-        Icon(imageResource(R.drawable.ic_baseline_add_24))
+        Icon(vectorResource(R.drawable.ic_baseline_add_24))
+    }
+}
+
+@Composable
+fun topBar(){
+    Row {
+        TopAppBar(modifier = Modifier.padding(8.dp)) {
+            Icon(vectorResource(id = R.drawable.ic_baseline_account_balance_24))
+            Icon(vectorResource(id = R.drawable.ic_baseline_print_24))
+            Icon(vectorResource(id = R.drawable.ic_baseline_help_outline_24))
+        }
+    }
+}
+
+
+@Composable
+fun HomeDrawer(){
+    val drawerItems = listOf(stringResource(id = R.string.diagrams), stringResource(id = R.string.help), stringResource(id = R.string.about_us))
+    Column{
+        LazyColumnFor(items = drawerItems) { item ->
+            Text(text = item, modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun bottomBar(){
+    BottomAppBar() {
+        Row {
+            TopAppBar(modifier = Modifier.padding(8.dp)) {
+                Icon(vectorResource(id = R.drawable.ic_baseline_account_balance_24))
+                Icon(vectorResource(id = R.drawable.ic_baseline_print_24))
+                Icon(vectorResource(id = R.drawable.ic_baseline_help_outline_24))
+            }
+        }
     }
 }
--- app/src/main/java/me/mehdi/mymoney/ui/MyMoneyTheme.kt
+++ app/src/main/java/me/mehdi/mymoney/ui/MyMoneyTheme.kt
@@ -1,9 +1,14 @@
 package me.mehdi.mymoney.ui
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.font
+import androidx.compose.ui.text.font.fontFamily
+import me.mehdi.mymoney.R
 
 @Composable
 fun MyMoneyTheme(content:@Composable () -> Unit){
-    MaterialTheme(content = content)
+    val typography = Typography(defaultFontFamily =  fontFamily(font(R.font.vazir)))
+    MaterialTheme(typography = typography, content = content)
 }
