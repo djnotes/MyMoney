baa9467dd0d283a6a516d294f6247c6a4c7d9036
mehdi <powergame_coder2@yahoo.com>
2020-11-06 01:22:02 -0500

    Added new item composable

--- app/src/main/java/me/mehdi/mymoney/MainActivity.kt
+++ app/src/main/java/me/mehdi/mymoney/MainActivity.kt
@@ -1,29 +1,20 @@
 package me.mehdi.mymoney
 
-import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawShadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageAsset
-import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import me.mehdi.mymoney.ui.MyMoneyTheme
@@ -31,7 +22,6 @@ import me.mehdi.mymoney.ui.MyMoneyTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         setContent{
             HomeScreen()
         }
@@ -55,9 +45,9 @@ fun HomeScreen(){
                 drawerScrimColor = Color.Green,
                 drawerContent = { HomeDrawer() }
         ){
-        Column(modifier = Modifier.fillMaxSize().drawShadow(8.dp)){
-            Text(modifier = Modifier.align(Alignment.CenterHorizontally).clip(RoundedCornerShape(16.dp)), text = stringResource(id = R.string.app_name))
-            Text(stringResource(id = R.string.here_is_your_costs))
+        Column(modifier = Modifier.fillMaxSize().drawShadow(4.dp)){
+            Text(modifier = Modifier.align(Alignment.CenterHorizontally).padding(8.dp), text = stringResource(id = R.string.app_name))
+            Text(stringResource(id = R.string.here_is_your_costs), modifier = Modifier.padding(PaddingValues(16.dp, 8.dp, 16.dp, 8.dp)).align(Alignment.CenterHorizontally))
         }
         }
     }
@@ -73,10 +63,11 @@ fun FAB(){
 @Composable
 fun topBar(){
     Row {
-        TopAppBar(modifier = Modifier.padding(8.dp)) {
-            Icon(vectorResource(id = R.drawable.ic_baseline_account_balance_24))
-            Icon(vectorResource(id = R.drawable.ic_baseline_print_24))
-            Icon(vectorResource(id = R.drawable.ic_baseline_help_outline_24))
+        TopAppBar(modifier = Modifier.padding(8.dp).align(Alignment.CenterVertically)) {
+            val iconModifier = Modifier.align(Alignment.CenterVertically).padding(4.dp)
+            Icon(modifier = iconModifier, asset = vectorResource(id = R.drawable.ic_baseline_account_balance_24))
+            Icon(vectorResource(id = R.drawable.ic_baseline_print_24), modifier = iconModifier)
+            Icon(vectorResource(id = R.drawable.ic_baseline_help_outline_24), modifier = iconModifier)
         }
     }
 }
@@ -102,5 +93,19 @@ fun bottomBar(){
                 Icon(vectorResource(id = R.drawable.ic_baseline_help_outline_24))
             }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun NewItem(){
+    var costName by   mutableStateOf("")
+    var costValue by mutableStateOf("")
+    val inputModifier = Modifier.padding(8.dp)
+
+    Column {
+        TextField(value = costName, onValueChange = { text -> costName = text }, label = { Text(stringResource(id = R.string.cost_name)) }, modifier = inputModifier, keyboardType = KeyboardType.Text)
+        TextField(value = costValue, onValueChange = { value -> costValue = value }, label = { Text(stringResource(id = R.string.cost_value)) }, modifier = inputModifier, keyboardType = KeyboardType.Number)
     }
 }
